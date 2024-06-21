ce04f6fe6217d5807e1178d4bc5e5ddb93723b46
sandoval <sandovalbgasantiago@gmail.com>
2024-06-20 21:23:51 -0500

    medio menu

--- betplay/src/main/java/com/betplay/Main.java
+++ betplay/src/main/java/com/betplay/Main.java
@@ -6,18 +6,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al sistema de la Liga BetPlay");
         boolean salida = false;
-        while (salida==false) {
-            System.out.println("LOGIN SI DESEA CANCELAR INGRESE X EN USUARIO");
-            try(Scanner sn = new Scanner(System.in)){
-                System.out.print("Usuario:\t");
-                String user = sn.nextLine();
-                if (user=="x") {
-                    salida=true;
-                }
-                System.out.print("Contrasena\t");
-                String pass = sn.nextLine();
-                
+        while (!salida) {
+            System.out.println("Bienvenido al Software LigaBetPlay");
+            System.out.println("1. Crear Equipo");
+            System.out.println("2. Crear Partido");
+            System.out.println("3. Ver Resultados");
+            System.out.println("4. Salir");
+            try(Scanner sc = new Scanner(System.in)){
+                System.out.println("Ingrese una opción:");
+                int opcion = sc.nextInt();
+                switch (opcion) {
+                    case 1:
+                    System.out.println("opcion 1");
+                    case 2:
+                    System.out.println("opcion 2");
+                    case 3:
+                    System.out.println("opcion 3");
+                    case 4:
+                    System.out.println("Adiós!");
             }
+        }
         }
     }
 }
